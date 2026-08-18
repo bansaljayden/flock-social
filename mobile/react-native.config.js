cef11da501e1fb84ee5106463450267d957cd1ff
Jayden Bansal <bansaljayden@gmail.com>
2026-08-18 14:29:28 -0400

    The abandoned React Native port leaves the repo; Capacitor is the launch path

--- mobile/react-native.config.js
+++
@@ -1,9 +0,0 @@
-// Links bundled font assets into the native iOS/Android builds.
-// After dropping the Satoshi .otf files into src/assets/fonts/, run:
-//   npx react-native-asset
-// which copies them in and registers UIAppFonts (iOS) + android assets.
-// typography.js references Satoshi-Light/Regular/Medium/Bold/Black.
-module.exports = {
-  project: { ios: {}, android: {} },
-  assets: ['./src/assets/fonts/'],
-};
